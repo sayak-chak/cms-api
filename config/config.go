0b0bdff91ec1a3990843de99d240dcd536947df2
Sayak Chakraborty <44857480+sayak-chak@users.noreply.github.com>
2021-07-18 21:08:00 +0100

    Add configs

---
+++ config/config.go
@@ -0,0 +1,7 @@
+package config
+
+const NumberOfTopContentsToConsider = 5
+
+var User = ""
+var DbName = ""
+var PostgresConfig = "postgres://sayakchakraborty@localhost:5432/postgres?sslmode=disable"
